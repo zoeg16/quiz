1cd3645e804c1423d730cf00ef4d5ba921ca6739
zoeg16 <143430669+zoeg16@users.noreply.github.com>
2024-03-14 16:19:02 +0000

    quiz

--- script.js
+++ script.js
@@ -1,3 +1,97 @@
 console.log("hello");
 
-let 
+let c1o1 = document.querySelector('.c1o1');
+let c1o2 = document.querySelector('.c1o2');
+let c2o1 = document.querySelector('.c2o1');
+let c2o2 = document.querySelector('.c2o2');
+let c3o1 = document.querySelector('.c3o1');
+let c3o2 = document.querySelector('.c3o2');
+let q1
+let q2
+let q3
+
+c1o1.addEventListener("click", function() {
+    q1=1
+    c1o1.style.border = "3px solid #white";
+});
+
+c1o2.addEventListener("click", function(){
+    q1=2
+    c1o2.style.border = "3px solid #white";
+});
+
+c2o1.addEventListener("click", function(){
+    q2=1
+    c2o1.style.border = "3px solid #white";
+});
+
+c2o2.addEventListener("click", function(){
+    q2=2
+    c2o2.style.border = "3px solid #white";
+});
+
+c3o1.addEventListener("click", function(){
+    q3=1
+    c3o1.stytle.border = "3px solid #white";
+});
+
+c3o2.addEventListener("click", function(){
+    q3=2
+    c3o2.style.border = "3px solid #white";
+});
+
+button.addEventListener("click", function(){
+    let message = document.querySelector("click", function();
+    if ( answer1 === "pink" && answer2 === "shopping" && answer3 === "water"){
+        message.innerHTML = " You Are Monica!";
+    } else if( answer1 === "pink" && answer2 === "shopping" && answer3 === "coffee"){
+        message.innerHTML = "You Are Rachel!";
+    } else if( answer1 === "pink" && answer2 === "sleeping" && answer3 === "water"){
+        message.innerHTML =" You Are Pheobe";
+    } else if(answer1 === "blue" && answer2 ===  "shopping" && answer3 === "water"){
+        message.innerHTML =" You are Chandler";
+    } else if(answer1 === "blue" && answer2 ===  "sleeping" && answer3 === "coffee"){
+        message.innerHTML = "You are Ross";
+    } else if(answer1 === "blue" && answer2 === "sleeping" && answer3 ===  "water"){
+        message.innerHTML = "You are Joey";
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
